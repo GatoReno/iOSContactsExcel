51239d6bf297cf4dee1e4b05dc2b65d2540b4a23
Ed <pushpoped@gmail.com>
2021-10-19 10:53:10 -0400

    Adding constraints and outlets

---
+++ ContactEx/AddContactViewController.swift
@@ -0,0 +1,54 @@
+//
+//  AddContactViewController.swift
+//  ContactEx
+//
+//  Created by ed on 10/18/21.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    var name: String?
+    var lastName: String?
+    var emaail: String?
+    var phone: String?
+    
+    @IBOutlet weak var contactNameTextField: UITextField!
+    
+    @IBOutlet weak var contactLastNameTextField: UITextField!
+    
+    @IBOutlet weak var contactEmailTextField: UITextField!
+    
+    @IBOutlet weak var contactPhoneTextField: UITextField!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        
+        
+    }
+    
+    func checkValues(){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ContactEx/ViewController.swift
+++ ContactEx/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        //navigate
+        self.performSegue(withIdentifier:"goToAddContact", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
